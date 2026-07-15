82f27e6b327fbf2e0750c20201b223bbea8b2261
barisaydogdu <barisaydogdu3@gmail.com>
2026-07-15 21:04:06 +0300

    Update: blueetooth whitelist added

--- internal/guard/guard_bluetooth.go
+++ internal/guard/guard_bluetooth.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 	"github/usb-guard-go/internal/util"
 	"log"
-	"os"
 	"os/exec"
-	"path/filepath"
 	"strings"
 )
 
@@ -15,40 +13,41 @@ type BluetoothGuard struct {
 }
 
 func (b *BluetoothGuard) Block(eventMap map[string]string) error {
-	if eventMap["DEVTYPE"] == "host" {
-		return nil
-	}
+	// if eventMap["DEVTYPE"] == "host" {
+	// 	return nil
+	// }
 
-	for key, value := range eventMap {
-		log.Printf("%s = %s\n", key, value)
-	}
+	// for key, value := range eventMap {
+	// 	log.Printf("%s = %s\n", key, value)
+	// }
 
-	addressPath := filepath.Join("/sys", eventMap["DEVPATH"], "address")
-	address, err := os.ReadFile(addressPath)
-	if err != nil {
-		return err
-	}
+	// addressPath := filepath.Join("/sys", eventMap["DEVPATH"], "address")
+	// address, err := os.ReadFile(addressPath)
+	// if err != nil {
+	// 	return err
+	// }
 
-	macAddress := strings.TrimSpace(string(address))
+	// macAddress := strings.TrimSpace(string(address))
 
-	for i := 0; i < len(b.Config.AllowedBluetoothMACs); i++ {
-		if b.Config.AllowedBluetoothMACs[i] == macAddress {
-			log.Printf("[Blueetooth] mac address is whitelisted. Allowing connection.\n")
-			return nil
-		}
-	}
+	// for i := 0; i < len(b.Config.AllowedBluetoothMACs); i++ {
+	// 	if b.Config.AllowedBluetoothMACs[i] == macAddress {
+	// 		log.Printf("[Blueetooth] mac address is whitelisted. Allowing connection.\n")
+	// 		return nil
+	// 	}
+	// }
 
-	cmd := exec.Command("rfkill", "block", "bluetooth")
-	err = cmd.Run()
-	if err != nil {
-		log.Println("Failed to block device:", err)
-		return err
-	}
+	// cmd := exec.Command("rfkill", "block", "bluetooth")
+	// err = cmd.Run()
+	// if err != nil {
+	// 	log.Println("Failed to block device:", err)
+	// 	return err
+	// }
 
-	log.Println("blocked blueetooth")
+	// log.Println("blocked blueetooth")
 
-	util.SendNotification("Device Blocked", fmt.Sprintf("An unauthorized device was detected and blocked.\nDevice ID: %s", macAddress))
+	// util.SendNotification("Device Blocked", fmt.Sprintf("An unauthorized device was detected and blocked.\nDevice ID: %s", macAddress))
 
+	// return nil
 	return nil
 }
 
@@ -63,8 +62,16 @@ func (b *BluetoothGuard) Allow(devPath string) error {
 }
 
 func (b *BluetoothGuard) BlockSpecificDevice(macAddress string) error {
+	fmt.Println("ccc2")
 	if macAddress == "" {
 		return fmt.Errorf("bluetooth mac address is empty")
+	}
+
+	for i := 0; i < len(b.Config.AllowedBluetoothMACs); i++ {
+		if b.Config.AllowedBluetoothMACs[i] == strings.TrimSpace(string(macAddress)) {
+			log.Printf("[USB] serial Number is whitelisted. Allowing connection.\n")
+			return nil
+		}
 	}
 
 	exec.Command("bluetoothctl", "disconnect", macAddress).Run()
@@ -73,7 +80,7 @@ func (b *BluetoothGuard) BlockSpecificDevice(macAddress string) error {
 
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		return fmt.Errorf("cihaz cannot block: %v, detail: %s", err, string(output))
+		return fmt.Errorf("device cannot block: %v, detail: %s", err, string(output))
 	}
 
 	log.Println("blocked blueetooth successfully")
